357951296ef647a6c0dea2608a505a8ce1575e99
narusai <narusai97@gmail.com>
2017-03-08 16:51:07 +0900

    Initial Commit

---
+++ voiceRecorder2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  voiceRecorder2
+//
+//  Created by Kazama Ryusei on 2017/03/08.
+//  Copyright © 2017年 Malfoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
